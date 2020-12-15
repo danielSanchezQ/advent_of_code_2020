b20080dd9c8d420d029fca428084c1708771aae3
danielsanchezq <daniel.sanchez@iohk.io>
2020-12-15 10:30:09 +0100

    Added 15th challenge

---
+++ advent15.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+initial_data = [1,20,11,6,12,0]
+
+
+class State(object):
+    __slots__ = ["visited", "counter", "i", "last"]
+
+    def __init__(self, input):
+        self.counter = defaultdict(int)
+        self.counter.update({k: c for c, k in enumerate(input[:-1], 1)})
+        self.i = len(input)
+        self.last = input[-1]
+
+    def iter_one(self):
+        self.i += 1
+        if self.last not in self.counter:
+            self.counter[self.last] = self.i - 1
+            self.last = 0
+        else:
+            prev_last_turn = self.counter[self.last]
+            self.counter[self.last] = self.i - 1
+            self.last = self.i - prev_last_turn - 1
+
+
+def solve(n, state):
+    while state.i < n:
+        state.iter_one()
+    return state.last
+
+
+if __name__ == "__main__":
+    state = State(initial_data)
+    print(solve(2020, state))
+    print(solve(30000000, state))
